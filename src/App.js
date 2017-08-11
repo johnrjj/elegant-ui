51810cc3708c8fd14ef56f28a76f1ff7268d8bcf
John Johnson <johnrjj@gmail.com>
2017-08-10 21:18:06 -0400

    Prettier

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 const palette = {
   offwhite: '#F6F6FA',
-}
+};
 
 const AppContainer = styled.div`
   position: relative;
@@ -33,29 +33,28 @@ const NavBarItem = styled.div`
   font-weight: 400;
   font-size: 0.85rem;
   margin: 1rem;
-  opacity: ${props => props.active ? 1 : 0.4};
+  opacity: ${props => (props.active ? 1 : 0.4)};
   cursor: pointer;
   padding: 1rem;
   :hover {
-    opacity: ${props => props.active ? 1 : 0.8};
+    opacity: ${props => (props.active ? 1 : 0.8)};
   }
 `;
 
-
-
 const PillButton = styled.button`
-  background-color: ${props => props.selected ? '#F9F9F9' : '#DD4DB3'};
+  background-color: ${props => (props.selected ? '#F9F9F9' : '#DD4DB3')};
   position: relative;
   vertical-align: middle;
   text-align: center;
-  padding: ${props => props.selected ? '0.75rem 1.5rem' : '1rem 1.5rem'};
+  padding: ${props => (props.selected ? '0.75rem 1.5rem' : '1rem 1.5rem')};
   line-height: 1rem;
   border-radius: 1.5rem;
   font-size: 0.7rem;
   text-transform: uppercase;
-  box-shadow: ${props => props.selected ? 'none' : 'rgba(221,77,179,0.30) 0 .25rem 1rem'};
+  box-shadow: ${props =>
+    props.selected ? 'none' : 'rgba(221,77,179,0.30) 0 .25rem 1rem'};
   letter-spacing: 1px;
-  color: ${props => props.selected ? '#DD4DB3' : '#fff'};
+  color: ${props => (props.selected ? '#DD4DB3' : '#fff')};
   border: none;
   min-width: 5.8rem;
   height: 50px;
@@ -72,8 +71,8 @@ const PillOutlineButton = styled.button`
   border-radius: 1.5rem;
   font-size: 1rem;
   letter-spacing: 1px;
-  color: #E44C7A;
-  border: 1px solid #E44C7A;
+  color: #e44c7a;
+  border: 1px solid #e44c7a;
   min-width: 5.8rem;
   cursor: pointer;
   opacity: 0.8;
@@ -84,7 +83,7 @@ const Card = styled.div`
   height: 18rem;
   width: 17rem;
   background-color: #fff;
-  box-shadow: rgba(95,42,80,0.10) 0 7px 42px;
+  box-shadow: rgba(95, 42, 80, 0.10) 0 7px 42px;
   border-radius: 0.5rem;
   overflow: hidden;
   padding: 1rem 0;
@@ -112,7 +111,7 @@ const StyledName = styled.div`
 `;
 
 const StyledPosition = styled.div`
-  color: #F054C0;
+  color: #f054c0;
   font-weight: 400;
   letter-spacing: 1px;
   font-size: 0.8rem;
@@ -120,32 +119,31 @@ const StyledPosition = styled.div`
   opacity: 0.8;
 `;
 
+const UserCard = ({ selected, avatarSrc, name, position }) =>
+  <Card>
+    <FlexContainer>
+      <Avatar src={avatarSrc} />
+      <div>
+        <StyledName>
+          {name}
+        </StyledName>
+        <StyledPosition>
+          {position}
+        </StyledPosition>
+      </div>
+      {selected
+        ? <PillButton selected>
+            <i className="material-icons">check</i>
+          </PillButton>
+        : <PillButton>Hire</PillButton>}
+    </FlexContainer>
+  </Card>;
+
 const LoadMoreButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   margin-bottom: 2rem;
 `;
-
-
-
-
-const UserCard = ({ selected, avatarSrc, name, position }) => (
-  <Card>
-    <FlexContainer>
-      <Avatar src={avatarSrc}></Avatar>
-      <div>
-        <StyledName>{name}</StyledName>
-        <StyledPosition>{position}</StyledPosition>
-      </div>
-      {
-        selected 
-          ? <PillButton selected><i className="material-icons">check</i></PillButton>
-          : <PillButton>Hire</PillButton>
-
-      }
-    </FlexContainer>
-  </Card>
-)
 
 const CardGrid = styled.div`
   display: grid;
@@ -154,30 +152,21 @@ const CardGrid = styled.div`
   margin-bottom: 4em;
 `;
 
-const UnstyledSearchIcon = ({ className }) => 
-  (<i className={ "material-icons " + className}>search</i>);
-
-const SearchIcon = styled(UnstyledSearchIcon)`
-  width: 2rem;
-  display: inline-block;
-  margin-right: 1rem;
-  opacity: 0.2;
-`;
-
 const ThinGrayLine = styled.hr`
   margin-left: 3rem;
   margin-bottom: 2rem;
-  background-color: #EEEEEE;
-  border-top: 1px solid #EEEEEE;
-  border-color: #EEEEEE;
+  background-color: #eeeeee;
+  border-top: 1px solid #eeeeee;
+  border-color: #eeeeee;
 `;
 
+// search
 const SearchContainer = styled.div`
   border: 1px solid #fff;
   border-radius: .5rem;
   max-width: calc(36rem);
   min-width: calc(17rem);
-  box-shadow: rgba(95,42,80,0.10) 0 7px 42px;
+  box-shadow: rgba(95, 42, 80, 0.10) 0 7px 42px;
   margin-bottom: 2rem;
   display: flex;
   align-items: center;
@@ -196,6 +185,16 @@ const SearchInput = styled.input`
   position: relative;
 `;
 
+const UnstyledSearchIcon = ({ className }) =>
+  <i className={'material-icons ' + className}>search</i>;
+
+const SearchIcon = styled(UnstyledSearchIcon)`
+  width: 2rem;
+  display: inline-block;
+  margin-right: 1rem;
+  opacity: 0.2;
+`;
+
 // sidenav
 const SideNav = styled.div`
   position: fixed;
@@ -206,7 +205,11 @@ const SideNav = styled.div`
   z-index: 0;
   padding-left: 1.5rem;
   color: #fff;
-  background: linear-gradient(to bottom, rgba(238,104,200,1) 0%,rgba(239,202,120,1) 100%); 
+  background: linear-gradient(
+    to bottom,
+    rgba(238, 104, 200, 1) 0%,
+    rgba(239, 202, 120, 1) 100%
+  );
 `;
 const SideNavTitle = styled.div`
   text-transform: uppercase;
@@ -233,59 +236,61 @@ const SideNavItemText = styled.div`
   font-size: 0.8rem;
   letter-spacing: 1px;
   margin-left: 0.5rem;
-
 `;
 
-const UnstyledIcon = ({ className, icon }) => 
-  (<i className={ "material-icons " + className}>{icon}</i>);
+const UnstyledIcon = ({ className, icon }) =>
+  <i className={'material-icons ' + className}>
+    {icon}
+  </i>;
 
 const SideNavIcon = styled(UnstyledIcon)`
   font-size: 1rem;
 `;
 
 const users = [
-  { 
+  {
     name: 'Angelica Montona',
-    avatar: 'https://randomuser.me/api/portraits/women/72.jpg', 
+    avatar: 'https://randomuser.me/api/portraits/women/72.jpg',
     position: 'UX/UI Designer',
     selected: false,
   },
-  { 
+  {
     name: 'Barny Stoner',
-    avatar: 'https://randomuser.me/api/portraits/men/79.jpg', 
+    avatar: 'https://randomuser.me/api/portraits/men/79.jpg',
     position: 'UX Expert',
-  },  
-  { 
+  },
+  {
     name: 'Yury Smirnov',
-    avatar: 'https://randomuser.me/api/portraits/men/64.jpg', 
+    avatar: 'https://randomuser.me/api/portraits/men/64.jpg',
     position: 'Design Lead',
     selected: true,
   },
-  { 
+  {
     name: 'John Newman',
-    avatar: 'https://randomuser.me/api/portraits/men/55.jpg', 
+    avatar: 'https://randomuser.me/api/portraits/men/55.jpg',
     position: 'Junior UX Designer',
   },
-  { 
+  {
     name: 'Linda Larson',
-    avatar: 'https://randomuser.me/api/portraits/women/55.jpg', 
+    avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
     position: 'Junior UX Designer',
   },
-  { 
+  {
     name: 'Erica Love',
-    avatar: 'https://randomuser.me/api/portraits/women/58.jpg', 
+    avatar: 'https://randomuser.me/api/portraits/women/58.jpg',
     position: 'UX Designer',
     selected: true,
   },
-  { 
+  {
     name: 'Barny Stoner',
-    avatar: 'https://randomuser.me/api/portraits/men/45.jpg', 
+    avatar: 'https://randomuser.me/api/portraits/men/45.jpg',
     position: 'UX Expert',
-  },    { 
+  },
+  {
     name: 'Barny Stoner',
-    avatar: 'https://randomuser.me/api/portraits/men/42.jpg', 
+    avatar: 'https://randomuser.me/api/portraits/men/42.jpg',
     position: 'UX Expert',
-  },  
+  },
 ];
 
 class App extends Component {
@@ -295,21 +300,21 @@ class App extends Component {
         <SideNav>
           <SideNavTitle>Elegant</SideNavTitle>
           <SideNavItemContainer>
-            <SideNavIcon icon={'dashboard'}/>
+            <SideNavIcon icon={'dashboard'} />
             <SideNavItemText>My shortlist</SideNavItemText>
           </SideNavItemContainer>
           <SideNavItemContainer>
-            <SideNavIcon icon={'class'}/>
+            <SideNavIcon icon={'class'} />
             <SideNavItemText>Proposals</SideNavItemText>
-          </SideNavItemContainer>          
+          </SideNavItemContainer>
           <SideNavItemContainer>
-            <SideNavIcon icon={'work'}/>
+            <SideNavIcon icon={'work'} />
             <SideNavItemText>Projects</SideNavItemText>
-          </SideNavItemContainer>         
-           <SideNavItemContainer>
-            <SideNavIcon icon={'event'}/>
+          </SideNavItemContainer>
+          <SideNavItemContainer>
+            <SideNavIcon icon={'event'} />
             <SideNavItemText>Calendar</SideNavItemText>
-          </SideNavItemContainer>          
+          </SideNavItemContainer>
         </SideNav>
         <MainContainer>
           <NavBar>
@@ -318,21 +323,20 @@ class App extends Component {
             <NavBarItem>Jobs</NavBarItem>
             <NavBarItem>Help</NavBarItem>
           </NavBar>
-          <ThinGrayLine/>
+          <ThinGrayLine />
           <SearchContainer>
-            <SearchInput placeholder="UX Designer"/>
-            <SearchIcon/>
+            <SearchInput placeholder="UX Designer" />
+            <SearchIcon />
           </SearchContainer>
           <CardGrid>
-            {
-              users.map(user => 
-                <UserCard
-                  selected={user.selected}
-                  avatarSrc={user.avatar}
-                  name={user.name}
-                  position={user.position}
-                ></UserCard>)
-            }
+            {users.map(user =>
+              <UserCard
+                selected={user.selected}
+                avatarSrc={user.avatar}
+                name={user.name}
+                position={user.position}
+              />
+            )}
           </CardGrid>
           <LoadMoreButtonContainer>
             <PillOutlineButton>Load More</PillOutlineButton>
@@ -344,5 +348,3 @@ class App extends Component {
 }
 
 export default App;
-
-
